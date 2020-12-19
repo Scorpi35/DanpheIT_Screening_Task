49a5307f85f95da164f4e506a33485be87ca277b
ashish <subediashish35@gmail.com>
2020-12-19 10:38:07 +0545

    Complete change color

--- Solution_1/solution1-web-app/src/App.js
+++ Solution_1/solution1-web-app/src/App.js
@@ -1,20 +1,60 @@
 import React, { Component } from "react"
-
 import "./App.css"
-
 import { connect } from "react-redux"
-
-import {
-  increaseCounter,
-  decreaseCounter,
-} from "./redux/Counter/counter.actions"
+import { increaseCounter, decreaseCounter } from "./redux/Counter/counter.actions"
 
 
 class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      colors: ["red", "green", "blue", "black", "orange"],
+      rendered_color: ""
+    }
+
+    this.changeColor = this.changeColor.bind(this);
+    this.selectColor = this.selectColor.bind(this);
+  }
+
+  changeColor(){
+    this.selectColor();
+  }
+
+  selectColor(){
+    let colors = this.state.colors;
+    let rendered_color = this.state.rendered_color;
+    let render_color = colors[Math.floor(Math.random() * colors.length)];
+    if(rendered_color === "blue"){
+      render_color = "green"
+    } 
+
+    this.setState({
+      rendered_color: render_color
+    })
+  }
+
+  componentDidMount(){
+    this.selectColor();
+  }
+
   render(){
+
+    let {rendered_color} = this.state;
+
     return(
       <div>
-        <button>Click Here</button>
+        <button 
+          onClick={this.changeColor} 
+          style={{backgroundColor: rendered_color, 
+            cursor: "pointer",
+            height: "50px",
+            width: "200px",
+            color: "white"
+          }}
+        >
+          Click to change the color
+        </button>
       </div>
     )
   }
@@ -29,7 +69,6 @@ const mapStateToProps = state => {
 const mapDispatchToProps = dispatch => {
   return {
     increaseCounter: () => dispatch(increaseCounter()),
-
     decreaseCounter: () => dispatch(decreaseCounter()),
   }
 }
